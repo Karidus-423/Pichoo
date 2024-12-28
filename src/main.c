c5d3de1089eaae62c78cbc7617b54709db296d3e
Karidus-423 <kapuerto23@gmail.com>
2024-12-28 10:56:36 -0500

    world to viewport

--- src/main.c
+++ src/main.c
@@ -2,39 +2,64 @@
 
 const float ASPECT_RATIO = 16.0 / 9.0;
 
+ahu hey;
+
+void FovTest(Camera3D *cam, float *fovDirection) {
+  float *fov = &cam->fovy;
+  float change = 0.5f;
+
+  *fov += change * (*fovDirection);
+
+  if (*fov <= 45.0f || *fov >= 90.0f) {
+    *fovDirection *= -1;
+  }
+  DrawSphere((Vector3){cam->position.x, cam->position.y, 0.5f}, 0.01f, RED);
+}
+
+void DrawPixelGrid(int win_width, int win_height) {}
+
 int main() {
   int width = 900;
   int height = (int)(width / ASPECT_RATIO);
+
   InitWindow(width, height, "Pichoo");
+
   SetTargetFPS(60);
 
   Camera3D camera = {0};
-  camera.position = (Vector3){0.0f, 2.0f, -5.0f};
-  camera.target = Vector3Zero();
-  camera.projection = CAMERA_PERSPECTIVE;
-  camera.fovy = 45.0f;
+  camera.position = (Vector3){0.0f, 2.0f, 0.0f};
+  camera.target = (Vector3){0.0f, 0.0f, 5.0f};
   camera.up = (Vector3){0.0f, 1.0f, 0.0f};
+  camera.fovy = 45.0f;
+  camera.projection = CAMERA_PERSPECTIVE;
+
+  SetTargetFPS(60);
+  Vector3 zpos = {0.0f, 0.0f, 10.0f};
+  float fovDirection = 1.0f;
 
   while (!WindowShouldClose()) {
-    GetWindowScaleDPI();
     UpdateCamera(&camera, CAMERA_FREE);
-    // Draw
-    //----------------------------------------------------------------------------------
+    if (IsKeyPressed('Z')) {
+      camera.target = (Vector3){0.0f, 0.0f, 0.0f};
+    };
+
     BeginDrawing();
-    BeginMode3D(camera);
     ClearBackground(SKYBLUE);
+    BeginMode3D(camera);
 
-    DrawSphere(
-        (Vector3){camera.position.x, camera.position.y, camera.position.z + 1},
-        0.02f, RED);
+    DrawCube(zpos, 1.0f, 1.0f, 1.0f, GREEN);
 
-    DrawCube((Vector3){0.0f, 0.0f, 10.0f}, 1, 1, 1, GREEN);
-    DrawGrid(20, 1);
+    DrawGrid(20, 1.0f);
+
+    // FovTest(&camera, &fovDirection);
+    DrawPixelGrid(width, height);
 
     EndMode3D();
+    DrawFPS(10, 10);
     EndDrawing();
   }
 
   CloseWindow();
+
   return 0;
 }
--- src/raytracer.h
+++ src/raytracer.h
@@ -4,7 +4,13 @@
 #include "../external/raylib-5.5/src/raylib.h"
 #include "../external/raylib-5.5/src/raymath.h"
 #include "../external/raylib-5.5/src/rlgl.h"
+
 #define RAYGUI_IMPLEMENTATION
+
 #include "../external/raygui-4.0/src/raygui.h"
+
+typedef struct ahu {
+  int ey;
+} ahu;
 
 #endif
